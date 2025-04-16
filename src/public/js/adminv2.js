882ff37b622d43e1661c727a965fc101f6c71501
hieu2642 <22022103@vnu.edu.vn>
2025-04-16 22:28:53 +0700

    adminv2.js

--- src/public/js/adminv2.js
+++ src/public/js/adminv2.js
@@ -24,22 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const areaChart = new Chart(document.getElementById('areaChart'), {
         type: 'line',
         data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+            labels: mostRecentlyMonth.label,
             datasets: [
                 {
                     label: 'Dataset 1',
-                    data: [100, 200, 300, 250, 400, 300],
+                    data: mostRecentlyMonth.users,
                     fill: true,
                     backgroundColor: 'rgba(111, 66, 193, 0.2)',
                     borderColor: '#6f42c1',
-                    tension: 0.4
-                },
-                {
-                    label: 'Dataset 2',
-                    data: [50, 150, 200, 300, 350, 200],
-                    fill: true,
-                    backgroundColor: 'rgba(0, 123, 255, 0.2)',
-                    borderColor: '#007bff',
                     tension: 0.4
                 }
             ]
@@ -57,17 +49,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const barChart = new Chart(document.getElementById('barChart'), {
         type: 'bar',
         data: {
-            labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            labels: mostRecentlyMonth.label,
             datasets: [
                 {
                     label: 'Dataset 1',
-                    data: [75, 150, 225, 100, 200, 150, 225, 75, 150, 225, 100, 200],
+                    data: mostRecentlyMonth.orders,
                     backgroundColor: '#007bff'
                 },
+                
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Line Chart
+    const lineChart = new Chart(document.getElementById('lineChart'), {
+        type: 'line',
+        data: {
+            labels: mostRecentlyMonth.label,
+            datasets: [
                 {
-                    label: 'Dataset 2',
-                    data: [50, 100, 150, 75, 150, 100, 150, 50, 100, 150, 75, 150],
-                    backgroundColor: '#6f42c1'
+                    label: 'Dataset 1',
+                    data: mostRecentlyMonth.revenue,
+                    fill: true,
+                    backgroundColor: '#072ef0',
+                    borderColor: '#6f42c1',
+                    tension: 0.4
                 }
             ]
         },
